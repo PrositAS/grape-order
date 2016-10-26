f35f3ad06fa2d2a1ca03495f921707088a540686
Grzegorz Brzezinka <grzegorz@brzezinka.eu>
2016-10-26 07:07:38 +0200

    add test coverage info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'simplecov'
+SimpleCov.start
 require 'rspec'
 require 'grape'
 require 'grape-order'
 require 'active_record'
 require 'rack/test'
+
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
