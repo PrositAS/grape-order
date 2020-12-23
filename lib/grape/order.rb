2a85f75be12adfb3b6e8f5763db27bd7d7d3a67a
jimi <lukasz@codebite.eu>
2020-12-23 12:12:46 +0100

    feat: update gem to handle new grape version

--- lib/grape/order.rb
+++ lib/grape/order.rb
@@ -1,30 +1,37 @@
-require "grape"
-require "grape/order/version"
+require 'grape'
+require 'grape/order/version'
 
 module Grape
-  module Order
-    def self.included(base)
-      base.class_eval do
-        helpers do
-          def order(collection)
-            sorters = params[:order].split(',').map do |s|
-              if s[0] == '-'
-                "#{s[1..-1]} asc"
-              else
-                "#{s} desc"
-              end
-            end
-            collection.order(sorters.join(','))
+  module Order # :nodoc:
+    extend ActiveSupport::Concern
+
+    included do
+      helpers HelperMethods
+    end
+
+    module HelperMethods # :nodoc:
+      extend Grape::API::Helpers
+
+      def order(collection)
+        sorters = params[:order].split(',').map do |s|
+          if s[0] == '-'
+            "#{s[1..-1]} asc"
+          else
+            "#{s} desc"
           end
         end
+        collection.order(sorters.join(','))
+      end
+    end
 
-        def self.order(sorter = nil)
-          params do
-            optional :order,     type: String, default: (sorter if sorter.present?),
-                     desc: 'Set sorting order.'
-          end
+    module DSLMethods # :nodoc:
+      def order(sorter = nil)
+        params do
+          optional :order, type: String, default: (sorter if sorter.present?),
+                           desc: 'Set sorting order.'
         end
       end
     end
+    Grape::API.const_defined?(:Instance) ? Grape::API::Instance.extend(DSLMethods) : Grape::API.extend(DSLMethods)
   end
 end
